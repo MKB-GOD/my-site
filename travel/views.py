13e91c786438f3fa708149edf7933fc2aac9d326
MKB-GOD <mhdyarkrymy92@gmail.com>
2026-07-19 13:00:15 +0330

    create render html files
    added template folder and html file to website app

--- travel/views.py
+++ travel/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home_page(requests):
-    return HttpResponse("<h1>homePage</h1>")
+def home_page(request):
+    return render(request, "website/index.html")
 
-def contact_page(requests):
-    return HttpResponse("<h1>contactPage</h1>")
+def contact_page(request):
+    return  render(request, "website/contact.html")
 
-def about_page(requests):
-    return HttpResponse("<h1>aboutPage</h1>")
+def about_page(request):
+    return  render("website/about.html")
