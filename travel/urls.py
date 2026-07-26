be593d7ec1428cfbc192353bd6a9eeeef34a6865
MKB-GOD <mhdyarkrymy92@gmail.com>
2026-07-26 11:18:40 +0330

    dynamic urls
    added dynamic urls with url template tag
    and with app-name = Travel

--- travel/urls.py
+++ travel/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from travel.views import *
+
+app_name = "Travel"
+
 urlpatterns = [
-    path('', home_page),
-    path('contact', contact_page),
-    path('about', about_page),
+    path('', home_page,name="home"),
+    path('contact', contact_page,name="contact"),
+    path('about', about_page,name="about"),
 ]
